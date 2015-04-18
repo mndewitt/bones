7dd44bb5a12347340ad4ce57008a50265f038c67
mndewitt <mdewitt07@gmail.com>
2015-04-18 11:33:18 -0400

    add sass grunt support, and watch

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+        sass: {                                  
+          dist: {                                  
+              files: [{
+              expand: true,
+              cwd: 'library/scss',
+              src: ['*.scss'],
+              dest: 'library/css',
+              ext: '.css'
+            }]
+         }
+      },
+      
+        watch: {                                                         
+            files: ['library/scss/**/*.scss'],
+            tasks: ['sass']     
+      }
+  });
+
+  //Load Grunt plugins
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  //Register the tasks
+  grunt.registerTask('default', ['sass']);
+
+
+}
